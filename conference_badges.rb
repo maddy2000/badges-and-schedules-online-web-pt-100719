720616242fe0b662cb650058366e5c7b8ee62f42
Madelyn Deshield <madelynwallop@gmail.com>
2019-11-04 01:35:54 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,35 @@
-# Write your code here.
+# define method using iterationand controlthe return values of methods
+#define methods that call other methods
+#print badges for speakers to read "hello my name is"
+
+
+
+
+
+def badge_maker(name)
+  badges = "Hello, my name is #{name}."
+  # puts "Hello,my name is #{name}."
+   #end
+ end
+   
+  def assign_rooms(names)
+    new_array=[]
+      names.each_with_index do |name, index|
+        new_array << "Hello, my name is #{name}. You'll be assigned to room #{index}!"
+  
+  end  
+  return new_array
+end
+
+def batch_badge_creator(names)
+  new_array=[]
+  names.each do |name|
+    new_array << badges "Hello, my name is #{names}."
+  end
+  return new_array
+end
+  
+  
+  
+  
+  
